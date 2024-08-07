ff269c372f3d7b900a9a73eebe4c656262435206
Julio_Mesquita <juliomartins@lampp-it.com.br>
2024-08-07 02:10:09 -0300

    wip - Adicionado paginação na consulta de event

--- src/main/java/com/juliomesquita/application/aggregate_context/infra/controllers/EventController.java
+++ src/main/java/com/juliomesquita/application/aggregate_context/infra/controllers/EventController.java
@@ -1,11 +1,18 @@
 package com.juliomesquita.application.aggregate_context.infra.controllers;
 
 import com.juliomesquita.application.aggregate_context.domain.entities.EventEntity;
+import com.juliomesquita.application.aggregate_context.domain.entities.SessionEntity;
 import com.juliomesquita.application.aggregate_context.infra.persistence.EventRepository;
 import com.juliomesquita.application.aggregate_context.infra.persistence.SessionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Limit;
+import org.springframework.data.domain.ScrollPosition;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Window;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.graphql.data.query.ScrollSubrange;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -26,5 +33,14 @@ public class EventController {
     @QueryMapping
     Optional<EventEntity> event(@Argument UUID id) {
         return this.eventRepository.findById(id);
+    }
+
+    @SchemaMapping
+    Window<SessionEntity> sessions(EventEntity event, ScrollSubrange subrange){
+        ScrollPosition position = subrange.position().orElse(ScrollPosition.offset());
+        Limit limit = Limit.of(subrange.count().orElse(10));
+        Sort sort = Sort.by("title").ascending();
+
+        return this.sessionRepository.findByEventId(event.getId(), position, limit, sort);
     }
 }
--- src/main/java/com/juliomesquita/application/aggregate_context/infra/persistence/SessionRepository.java
+++ src/main/java/com/juliomesquita/application/aggregate_context/infra/persistence/SessionRepository.java
@@ -1,10 +1,16 @@
 package com.juliomesquita.application.aggregate_context.infra.persistence;
 
 import com.juliomesquita.application.aggregate_context.domain.entities.SessionEntity;
+import org.springframework.data.domain.Limit;
+import org.springframework.data.domain.ScrollPosition;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Window;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
+
 @Repository
 public interface SessionRepository extends JpaRepository<SessionEntity, UUID> {
+    Window<SessionEntity> findByEventId(UUID id, ScrollPosition position, Limit limit, Sort sort);
 }
